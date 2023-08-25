de4b9b4cf3432b03e7ed1b16228a8c1c860ce286
Karla Urrea <60410533+Karldin11@users.noreply.github.com>
2023-08-25 00:35:54 -0700

    End of section 4

--- API/Program.cs
+++ API/Program.cs
@@ -17,6 +17,8 @@ builder.Services.AddDbContext<StoreContext>(opt =>
 });
 
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 var app = builder.Build();
 
@@ -26,6 +28,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseStaticFiles();
 
 app.UseHttpsRedirection();
 
@@ -44,7 +48,7 @@ try
 }
 catch (Exception ex)
 {
-    logger.LogError(ex, "An error occured during migration");
+    logger.LogError(ex, "An error occurred during migration");
 }
 
 
